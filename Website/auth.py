28850e23b66a019dbef3fe414c03fe3a45369ced
memadhavkh <144777509+memadhavkh@users.noreply.github.com>
2024-04-07 12:09:22 +0530

    Final Commit
    Final Commit

--- Website/auth.py
+++ Website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from Flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
 from flask_login import login_user, logout_user , login_required, current_user
